c1876cf21b70b045dd89aab23e230e45f974b73a
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2023-08-15 13:00:55 -0600

    Day 1

---
+++ Day-1-Band-Name-Generator/Band-Name-Generator.py
@@ -0,0 +1,15 @@
+#Created a greeting the program.
+print("Welcome to the band name generator!")
+print("Follow the prompts below to create a band name.\n")
+
+#Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+#Ask the user for the name of a pet.
+pet = input("What was the name of your favorite pet?\n")
+
+#Combine the name of their city and pet and show them their band name.
+name = (city + " " + pet)
+
+#Make sure the input cursor shows on a new line:
+print("Your band name can be " + name + "!")
