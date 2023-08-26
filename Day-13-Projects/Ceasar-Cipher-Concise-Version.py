df3b398dd332d963278cf1fabbe23b97e8616749
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2023-08-26 11:02:07 -0600

    test

---
+++ Day-13-Projects/Ceasar-Cipher-Concise-Version.py
@@ -0,0 +1,76 @@
+'''
+TITLE: Ceasar's Cipher - Concise Version
+DESCRIPTION: What is Caesar's Cipher: 
+  - An encryption and decryption technique. It takes a message and encrypts it by shifting the letters
+    in the message by a specific position. To decrypt the message the person needs the encrypted message
+    and the specific shift number. 
+USED: Defining a function with parameters and arguments, .index(), calling specific values in lists
+
+'''
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z'
+]
+
+def caesar(direction, text, shift):
+  
+  cipher_text = ""
+  for letter in text:
+    if letter in alphabet: 
+      position = alphabet.index(letter)
+
+      if direction == "encode":
+        new_position = int(position + shift)
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+      
+      elif direction == "decode":
+        new_position = int(position - shift)
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    else:
+      cipher_text += letter
+      
+  print(f"The {direction}d text is: {cipher_text}\n\n")
+
+print(logo)
+
+play = True
+
+while play:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+
+  caesar(direction, text, shift)
+
+  play_again = input("Type 'yes' if you want to play again. Otherwise type 'no'. \n")
+  if play_again == "no":
+    play = False
+    print("Goodbye")
