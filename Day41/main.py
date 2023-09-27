67a3868be61db5211659a855b63cc28a5c28c6ec
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2023-09-26 20:36:17 -0600

    Completed

---
+++ Day41/main.py
@@ -0,0 +1,31 @@
+#Option-1
+#Turns the weather_data into a list
+# with open("Day41/weather_data.csv") as data_file:
+#     weather_data = data_file.readlines()
+#     print(weather_data)
+
+
+
+#Option-2
+#Built-in library
+# import csv
+
+# with open("Day41/weather_data.csv") as data_file:
+#     #reader() can read it and output data
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     #Turns the data into a list by row
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+
+
+
+import pandas
+
+data = pandas.read_csv("Day41/weather_data.csv")
+#Pandas gives a nice row of the data
+# print(data)
+
+#It takes the first row and then applies it to the rest
+print(data["temp"])
