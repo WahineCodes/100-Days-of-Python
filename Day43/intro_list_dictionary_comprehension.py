bc6a216d48815c5c5f4063152c3ffb4f8c02e82d
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2023-09-28 19:43:09 -0600

    Completed

--- Day43/intro_list_dictionary_comprehension.py
+++ Day43/intro_list_dictionary_comprehension.py
@@ -42,3 +42,25 @@ print(student_data_frame)
 for (index, row) in student_data_frame.iterrows():
     if row.student == "Angela":
         print(row.score)
+
+
+
+'''Reference
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+'''
