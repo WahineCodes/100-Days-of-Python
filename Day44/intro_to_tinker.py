0a8267a2e90d3f08f69b4a03e71d2a0137e75ef6
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2023-09-29 20:56:49 -0600

    Completed

--- Day44/intro_to_tinker.py
+++ Day44/intro_to_tinker.py
@@ -14,8 +14,5 @@ my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
 #Specify where the label will be on the screen
 my_label.pack(side="left", expand = True)
 
-
-
-
 #Keeps window on the screen like a loop. Needs to stay at end of the program
 window.mainloop()
