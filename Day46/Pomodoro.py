bb949922247686666484a6fb67a835ca3fb1081e
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2023-11-07 14:04:14 -1000

    Completed

--- Day46/Pomodoro.py
+++ Day46/Pomodoro.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,26 +10,60 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-def reset_clicked():
+def reset_timer():
     pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-def start_clicked():
-    pass
+def start_timer():
+    global reps
+    reps += 1
+
+    # work_sec = WORK_MIN * 60
+    # short_break_sec = SHORT_BREAK_MIN * 60
+    # long_break_sec = LONG_BREAK_MIN * 60
+
+    work_sec = WORK_MIN 
+    short_break_sec = SHORT_BREAK_MIN 
+    long_break_sec = LONG_BREAK_MIN 
+
+    #If its the 8th rep
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+
+    #If it's the 2nd/4th/6th rep
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    print(count)
-    window.after(1000, count_down, count-1)
+
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+
+    #Dynamic Typing Example (int then str)
+    #Dynamic = runtime objects(values) have a type, as opposed to static typing variables have a type
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-
-#The number countdown by
-count_down(5)
 
 #Timer Label
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
@@ -38,15 +73,15 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="Day46/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(103, 112, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(103, 112, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 #Start Button
-start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW, command=start_clicked)
+start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW, command=start_timer)
 start_button.grid(column=0,row=2)
 
 #Reset Button
-reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW, command=reset_clicked)
+reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 #Check Mark
