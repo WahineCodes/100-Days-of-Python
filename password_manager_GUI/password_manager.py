8b7751e1a5af01759a26f2dcbfe6330578e65379
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2024-01-25 09:19:54 -1000

    Update password_manager.py

--- password_manager_GUI/password_manager.py
+++ password_manager_GUI/password_manager.py
@@ -1,14 +1,47 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 #Note: Command + / for = # block of code
 
-# ---------------------------- Global Variables ------------------------------- #
-
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def make_password():
-    pass
+    #List of random letters, symbols, and numbers. 
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] #0-51
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+'] #0-8
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] #0-9
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    #Example of List Comprehension
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+   
+    #Example of generating password with for loops
+    # for char in range(nr_letters):
+    #     password_list.append(random.choice(letters))
+    
+    # for char in range(nr_symbols):
+    #     password_list += (random.choice(symbols))
+
+    # for char in range(nr_numbers):
+    #     password_list += random.choice(numbers)
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    #Example 1 of password list
+    # password = ""
+    # for char in password_list:
+    #     password += char
+
+    #Example 2
+    password = "".join(password_list)
+    
+    print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 #When clicking the add button
@@ -17,17 +50,18 @@ def save_password():
     email = email_input.get()
     password = password_input.get()
 
-    #Standard Dialog - for a pop-up to check details of their inputs
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
-    
-    if is_ok: 
-        with open("password_manager_GUI/data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
-            #deleting from 0 character to the end of the entry, so it can take the next entry.
-            website_input.delete(0, END)
-            password_input.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty")
+    else:
+        #Standard Dialog - for a pop-up to check details of their inputs.
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+        if is_ok: 
+            with open("password_manager_GUI/data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
 
-
+                #deleting from character 0 to the end of the entry, for new entries to be entered.
+                website_input.delete(0, END)
+                password_input.delete(0, END)
         
 
 # ---------------------------- UI SETUP ------------------------------- #
