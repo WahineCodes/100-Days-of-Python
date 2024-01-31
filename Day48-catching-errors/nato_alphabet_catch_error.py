62ed8a79551140c60d382e242286e4b2162a6a70
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2024-01-31 10:38:18 -1000

    Update nato_alphabet_catch_error.py

--- Day48-catching-errors/nato_alphabet_catch_error.py
+++ Day48-catching-errors/nato_alphabet_catch_error.py
@@ -1,6 +1,8 @@
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
+#Objective: Update the Nato Alphabet Alphabet converter to catch errors if uses input anything but a letter. 
+
 import pandas
 
 data = pandas.read_csv("Day48-catching-errors/nato_phonetic_alphabet.csv")
@@ -8,11 +10,16 @@ data = pandas.read_csv("Day48-catching-errors/nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
 
-try:
-    output_list = [phonetic_dict[letter] for letter in word]
-except KeyError:
-    print("Sorry, only letters in the alphabet please")
-else:
-    print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
