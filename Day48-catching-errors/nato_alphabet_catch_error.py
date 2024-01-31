c4348aaf559efa64bb33335cd7af1a4c1150c7f5
WahineCodes <124655841+WahineCodes@users.noreply.github.com>
2024-01-31 10:35:48 -1000

    Update nato_alphabet_catch_error.py

--- Day48-catching-errors/nato_alphabet_catch_error.py
+++ Day48-catching-errors/nato_alphabet_catch_error.py
@@ -8,13 +8,11 @@ data = pandas.read_csv("Day48-catching-errors/nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
+word = input("Enter a word: ").upper()
 
 try:
-    word = input("Enter a word: ").upper()
-except:
-    
+    output_list = [phonetic_dict[letter] for letter in word]
+except KeyError:
     print("Sorry, only letters in the alphabet please")
-    word = input("Enter a word: ").upper()
-
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+else:
+    print(output_list)
